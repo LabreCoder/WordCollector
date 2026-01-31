f178905a87bf878fe007d7cc38a797aac4e5176f
LabreCoder <jvlabremachado@gmail.com>
2026-01-30 21:35:41 -0300

    Realizei algumas alterações no Bash para ele sempre buscar a pasta onde o arquivo está sendo executado, evitando conflitos

--- bash/scripts/email_monitoring.sh
+++ bash/scripts/email_monitoring.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-BASE_DIR="$HOME/Documents/Programming/Python/SMTP/send_new_english_words"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+BASE_DIR="$(cd "$SCRIPT_DIR/../.." && pwd)"
+
 LOG_DIR="$BASE_DIR/bash/logs"
 LOG_ARQ="$LOG_DIR/logs_email_monitoring.txt"
 DATE=$(date +"%Y-%m-%d %H:%M:%S")
 
 mkdir -p "$LOG_DIR"
-touch "$LOG_ARQ"
-
 python3 "$BASE_DIR/send_email.py"
-STATUS=$?
+
+STATUS=$? # Espera retorno da função python informando se teve sucesso ou não ao enviar o e-mail
 
 if [ $STATUS -eq 0 ]; then
     echo "[$DATE] INFO: Processo executado com sucesso" >> "$LOG_ARQ"
